4f8ca67b39b26749ec1870734f20315c2fa4e7de
Idoko Agbo <idokojudge@gmail.com>
2022-01-05 00:34:37 -0500

    refactor: abstract navbar to seperate component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 import Marketplace from '../abis/MarketPlace.json';
+import Navbar from './Navbar';
 
 class App extends Component {
 
@@ -55,22 +56,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Al-Ghoul
-          </a>
-        </nav>
+        <Navbar />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="#"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -82,7 +74,7 @@ class App extends Component {
                 </p>
                 <a
                   className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="#"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
